d7e6e834a427915f336d4e17654c722b1285dd35
kevin <lxk@yhkodia.com>
2017-10-01 15:18:44 +0800

    --业务逻辑测试

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var User = reuqire('../models/users')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//reg 注册信息处理逻辑
+
+router.post('/reg', function(req, res, next) {
+	  var b = req.body;
+	  var _user={
+	    username: b.username,
+	    password: b.password
+	  }
+	  var user=new User(_user);
+	  user.save(function(err,user){
+	  if (err) {
+	  console.log(err);
+	  return;
+	  }
+	  console.log('注册成功');
+	  res.send('注册成功');
+	  })
+	});
 
 module.exports = router;
