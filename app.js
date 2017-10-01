59b0ece21619e20bf5f11872f39264f2c868c2e1
kevin <lxk@yhkodia.com>
2017-10-01 15:49:16 +0800

    {useMongoClient: true}

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var users = require('./routes/users');
 
 var app = express();
 //远程数据库
-mongoose.connect('mongodb://root:UIrbWa9cvF3PK3gSI4qjbhZhU52zeUDSuQI4Oc73@ermkmolebuhn.mongodb.sae.sina.com.cn:10479/fuck_sae');
+mongoose.connect('mongodb://root:UIrbWa9cvF3PK3gSI4qjbhZhU52zeUDSuQI4Oc73@ermkmolebuhn.mongodb.sae.sina.com.cn:10479/fuck_sae',{useMongoClient: true});
 //本地数据库
-//mongoose.connect('mongodb://127.0.0.1:27017/test');
+//mongoose.connect('mongodb://127.0.0.1:27017/test',{useMongoClient: true});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
