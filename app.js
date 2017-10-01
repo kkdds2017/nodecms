c62cd57fe42eeacc0dc36c998bb7466e5697cbe1
kevin <lxk@yhkodia.com>
2017-10-01 15:34:43 +0800

    -fuck_sae uri

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 
 var app = express();
 //远程数据库
-#mongoose.connect('mongodb://root:UIrbWa9cvF3PK3gSI4qjbhZhU52zeUDSuQI4Oc73@ermkmolebuhn.mongodb.sae.sina.com.cn:10479,lioswkgirsvj.mongodb.sae.sina.com.cn:10479/fuck_sae');
+#mongoose.connect('mongodb://root:UIrbWa9cvF3PK3gSI4qjbhZhU52zeUDSuQI4Oc73@ermkmolebuhn.mongodb.sae.sina.com.cn:10479/fuck_sae');
 //本地数据库
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 // view engine setup
